8d5549c783d0be19b4cbd239bb70f040d5af473d
Juan Saavedra <yellowitsmijuanii@gmail.com>
2021-12-01 23:46:22 -0300

    second commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return <>
+        <div id="Navbar">
+            <nav>
+                <img src="/logo192.png" alt="" />
+            </nav>
+        </div>
+        
+
+    </>
+    
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//1) Necesitamos la variable React en scope
+//import React from "react"
+
+
+
+
+//2) Necesitamos la variable ReactDOM en scope
+import ReactDOM from "react-dom" 
+
+
+
+//3) Necesitamos una aplicaciond e reat
+//function App(){return "Hola mundo"} es lo mism
+//4) Necesitamos dibujar la aplicacion en el DOM
+ReactDOM.render(<App/>, document.getElementById("root"))
